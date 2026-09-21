b508c394aa2ac60757c61ec3784ef270e7efdf87
target2212-gif <target2212@gmail.com>
2026-09-21 13:26:47 +0300

    Delete Alrawasi_CRM_OneClick_Deploy directory

--- Alrawasi_CRM_OneClick_Deploy/sync_cron.py
+++
@@ -1,6 +0,0 @@
-import os, requests, sys
-base=os.environ["CRM_BASE_URL"].rstrip("/")
-token=os.environ["MUQAWIL_SYNC_TOKEN"]
-r=requests.post(base+"/internal/sync-muqawil",headers={"X-Sync-Token":token},timeout=900)
-print(r.status_code, r.text)
-r.raise_for_status()
